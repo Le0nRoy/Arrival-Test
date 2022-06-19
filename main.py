1d4de3fa7abfc6eab0b215284619885efdac0753
le0n <cerber9283@gmail,com>
2022-06-19 17:17:31 +0400

    Finished library (not debugged yet)

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import requests
 
-request_url_base = "http://localhost:8099/api"
+REQUEST_URL_BASE = "http://localhost:8099/api"
+
+
+class APIError(AssertionError):
+    def get_message(self):
+        return self.__str__()
+
+
+def send_request(request_url: str, request_method: str = "get", data: dict = None,
+                 json_data: dict = None) -> requests.Response:
+    if request_method.lower() == "get":
+        r = requests.get(request_url)
+    elif request_method.lower() == "post":
+        if type(data) is dict and json_data is None:
+            r = requests.post(request_url, data=data)
+        elif type(json_data) is dict and data is None:
+            r = requests.post(request_url, json=json_data)
+        else:
+            raise ValueError(f"No data was provided for request_method `{request_method}`")
+    else:
+        raise ValueError(f"Unknown request_method `{request_method}` was passed to function")
+
+    if r.status_code != 200:
+        raise APIError(f"Error occurred while sending request '{request_url}'")
+    return r
 
 
 class Signals:
@@ -10,6 +34,16 @@ class Signals:
     ReqTorque = 4
     BatteryState = 5
 
+    @staticmethod
+    def get_signal_state(signal_id: int) -> str:
+        request_url = f"{REQUEST_URL_BASE}/signals/{signal_id}"
+        return send_request(request_url).json()['Value']
+
+    @staticmethod
+    def get_all_signals_states() -> dict:
+        request_url = f"{REQUEST_URL_BASE}/signals"
+        return send_request(request_url).json()
+
 
 class Pins:
     Gear_1 = 1
@@ -18,22 +52,22 @@ class Pins:
     BrakePedal = 4
     BatteryVoltage = 5
 
+    @staticmethod
+    def get_pin_voltage(pin_id: int) -> float:
+        request_url = f"{REQUEST_URL_BASE}/pins/{pin_id}"
+        return send_request(request_url).json()['Voltage']
 
-def get_signal_state(signal) -> str:
-    request_url = f"{request_url_base}/signals/{signal}"
-    return requests.get(request_url).json()['Value']
+    @staticmethod
+    def get_all_pins_voltages() -> dict:
+        request_url = f"{REQUEST_URL_BASE}/pins"
+        return send_request(request_url).json()
 
+    @staticmethod
+    def set_pin_voltage(pin_id: int, voltage: float):
+        request_url = f"{REQUEST_URL_BASE}/pins/{pin_id}/update_pin"
+        send_request(request_url, data={'Voltage': voltage})
 
-def get_all_signals_states() -> dict:
-    request_url = f"{request_url_base}/signals"
-    return requests.get(request_url).json()
-
-
-def get_pin_voltage(pin) -> float:
-    request_url = f"{request_url_base}/pins/{pin}"
-    return requests.get(request_url).json()['Voltage']
-
-
-def get_all_pins_voltages() -> dict:
-    request_url = f"{request_url_base}/pins"
-    return requests.get(request_url).json()
+    @staticmethod
+    def set_multiple_pins_voltage(json_data: dict):
+        request_url = f"{REQUEST_URL_BASE}/pins/update_pin"
+        send_request(request_url, json_data=json_data)
